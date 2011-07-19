75e60e1f9d63a5c479ec08dabb2b8226974723f6
eddy <eddy@quazar.(none)>
2011-07-19 18:44:56 +0300

    task #3 implementation

--- task-3/log.h
+++ task-3/log.h
@@ -2,28 +2,39 @@
 #define __LOG_H__
 
 #include <linux/kernel.h>
+#include <linux/version.h>
 #include <linux/ratelimit.h>
 
 #define LOG(format, ...) \
     printk(KERN_DEBUG "%s:%d " format "\n", __FUNCTION__, __LINE__, ##__VA_ARGS__)
 
-#ifdef BOO
+#define ERR(format, ...) \
+    printk(KERN_ERR "%s:%d:%s " format "\n", __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__)
 
-#define LOGRL(format, ...) \
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 38)
+
+#define LOG_RL(format, ...) \
     printk_ratelimited(KERN_DEBUG "%s:%d " format "\n", __FUNCTION__, __LINE__, ##__VA_ARGS__)
+
+#define ERR_RL(format, ...) \
+    printk_ratelimited(KERN_ERR "%s:%d:%s " format "\n", __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__)
 
 #else
 
-#define LOGRL(format, ...)          \
+#define LOG_RL(format, ...)          \
     do {                            \
         if (printk_ratelimit()) {   \
             LOG(format, ##__VA_ARGS__);     \
         }                           \
     } while (0)
 
-#endif
+#define ERR_RL(format, ...)          \
+    do {                            \
+        if (printk_ratelimit()) {   \
+            ERR(format, ##__VA_ARGS__);     \
+        }                           \
+    } while (0)
 
-#define ERR(format, ...) \
-    printk(KERN_ERR "%s:%d:%s " format "\n", __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__)
+#endif
 
 #endif /* __LOG_H__ */
--- task-3/t2-main.c
+++ task-3/t2-main.c
@@ -4,47 +4,228 @@
 #include <linux/timer.h>
 #include <linux/interrupt.h>
 #include <linux/list.h>
-
 #include <linux/slab.h>
-//#include <linux/workqueue.h>
-//#include <linux/kthread.h>
-//#include <linux/completion.h>
+#include <linux/random.h>
+#include <linux/kthread.h>
+#include <linux/mutex.h>
 //#include <linux/percpu.h>
+
+#include "log.h"
 
 // need this for mouse interrupt number: I8042_AUX_IRQ -- PS/2 mouse
 //#include <drivers/input/serio/i8042-io.h>
 #define I8042_AUX_IRQ 12
 
-/*
- TODO:
-    - spin locks
-    - vmalloc/kmalloc
-    - sem up/down
- */
-
-#include "log.h"
 
 MODULE_LICENSE("GPL");
 
+#define TIMER_PERIOD_MSEC    10
+#define TIMER_DELAY_MSEC     10
+#define THREAD_PERIOD_MSEC   10
+
+#define mod_timer_ms(timer, msec) \
+    mod_timer((timer), msecs_to_jiffies(msec))
+
+#define RC_SIZE 10
+
+#define NULLIFY(ptr) memset((ptr), 0, sizeof(typeof(*(ptr))))
+
+typedef int common_resource[RC_SIZE];
+
 struct context {
     struct list_head list;
+    
     int irq_registered;
+    
+    atomic_t cycles_count;
+    atomic_t failures_count;
+    atomic_t mouse_irqs_count;
+    atomic_t thread_syncs_count;
+
+    struct timer_list single_shot_timer;
+    struct timer_list periodic_timer;
+
+    common_resource rc_irq;
+    common_resource rc_tasklet;
+    common_resource rc_irq_tasklet;
+    common_resource rc_thread;
+    common_resource rc_tasklet_thread;
+
+    spinlock_t irq_lock;
+    spinlock_t tasklet_lock;
+    spinlock_t irq_tasklet_lock;
+    spinlock_t tasklet_thread_lock;
+    struct mutex thread_lock;
+    
+    struct tasklet_struct tasklet_a;
+    struct tasklet_struct tasklet_b;
+
+    struct task_struct *thread_a;
+    struct task_struct *thread_b;
 };
 
 static LIST_HEAD(contexts);
 
-#define DEF_CONTEXT(name, arg) struct context* name = (struct context*)(arg)
+unsigned int get_random_int(void) {
+    //unsigned int v;
+    //get_random_bytes(&v, sizeof(v));
+    //return v;
 
-irqreturn_t mouse_irq(int irq, void *arg) {
+    static atomic_t v = ATOMIC_INIT(0);
+    atomic_inc(&v);
+    return atomic_read(&v);
+}
+
+static void write_rc(struct context *ctx, common_resource rc, const char *name) {
+    int i;
+    int v;
+    int sum1 = 0;
+    int sum2 = 0;
+    int n = RC_SIZE;
+
+    for (i = 0; i < n; ++i) {
+        v = get_random_int();
+        rc[i] = v;
+        sum1 += v;
+    }
+
+    for (i = 0; i < n; ++i) {
+        sum2 += rc[i];
+    }
+
+    atomic_inc(&ctx->cycles_count);
+
+    if (sum1 != sum2) {
+        atomic_inc(&ctx->failures_count);
+        ERR_RL("sync failure in: %s", name);
+    }
+
+    LOG_RL("count is: %i/%i/%i/%i", atomic_read(&ctx->cycles_count),
+                                    atomic_read(&ctx->failures_count),
+                                    atomic_read(&ctx->mouse_irqs_count),
+                                    atomic_read(&ctx->thread_syncs_count));
+}
+
+static void write_rc_irq(struct context *ctx, const char *name) {
+    unsigned long flags;
+    
+    spin_lock_irqsave(&ctx->irq_lock, flags);
+    write_rc(ctx, ctx->rc_irq, name); 
+    spin_unlock_irqrestore(&ctx->irq_lock, flags);
+    
+    spin_lock_irqsave(&ctx->irq_tasklet_lock, flags);
+    write_rc(ctx, ctx->rc_irq_tasklet, name); 
+    spin_unlock_irqrestore(&ctx->irq_tasklet_lock, flags);
+}
+
+#define DEF_CONTEXT(name, arg) \
+    struct context* name = (struct context*)(arg)
+
+static void timer_func(unsigned long arg) {
     DEF_CONTEXT(ctx, arg);
-    LOGRL("got mouse interrupt: %i:%p", irq, ctx);
+    write_rc_irq(ctx, __FUNCTION__);
+}
+
+static void periodic_timer_func(unsigned long arg) {
+    DEF_CONTEXT(ctx, arg);
+    write_rc_irq(ctx, __FUNCTION__);
+    mod_timer_ms(&ctx->periodic_timer, TIMER_PERIOD_MSEC);
+}
+
+static irqreturn_t mouse_irq(int irq, void *arg) {
+    DEF_CONTEXT(ctx, arg);
+
+    atomic_inc(&ctx->mouse_irqs_count);
+    //LOG_RL("got mouse interrupt: %i:%p", irq, ctx);
+    
+    switch (get_random_int() % 4) {
+    case 0:
+        mod_timer_ms(&ctx->single_shot_timer, TIMER_DELAY_MSEC);
+        break;
+    case 1:
+        tasklet_schedule(&ctx->tasklet_a);
+        break;
+    case 2:
+        tasklet_schedule(&ctx->tasklet_b);
+        break;
+    default:
+        write_rc_irq(ctx, __FUNCTION__);
+    }
+
     return IRQ_NONE;
 }
 
+static void write_rc_tasklet(struct context *ctx, const char *name) {
+    unsigned long flags;
+    
+    spin_lock(&ctx->tasklet_lock);
+    write_rc(ctx, ctx->rc_tasklet, name); 
+    spin_unlock(&ctx->tasklet_lock);
+    
+    spin_lock_irqsave(&ctx->irq_tasklet_lock, flags);
+    write_rc(ctx, ctx->rc_irq_tasklet, name); 
+    spin_unlock_irqrestore(&ctx->irq_tasklet_lock, flags);
+    
+    spin_lock(&ctx->tasklet_thread_lock);
+    write_rc(ctx, ctx->rc_tasklet_thread, name); 
+    spin_unlock(&ctx->tasklet_thread_lock);
+}
+
+static void tasklet_func(unsigned long arg) {
+    DEF_CONTEXT(ctx, arg);
+    write_rc_tasklet(ctx, __FUNCTION__);
+}
+
+static void write_rc_thread(struct context *ctx, const char *name) {
+    atomic_inc(&ctx->thread_syncs_count);
+
+    mutex_lock(&ctx->thread_lock);
+    write_rc(ctx, ctx->rc_thread, name); 
+    mutex_unlock(&ctx->thread_lock);
+    
+    spin_lock_bh(&ctx->tasklet_thread_lock);
+    write_rc(ctx, ctx->rc_tasklet_thread, name); 
+    spin_unlock_bh(&ctx->tasklet_thread_lock);
+}
+
+static int thread_func(void *arg) {
+    DEF_CONTEXT(ctx, arg);
+
+    while (!kthread_should_stop()) {
+        write_rc_thread(ctx, __FUNCTION__);
+        msleep_interruptible(THREAD_PERIOD_MSEC); // could be awaken by stop call
+    }
+
+    return 0;
+}
+
+#define callback(ctx, action, field, func) \
+    action(&((ctx)->field), (func), (unsigned long) (ctx))
+
 static void init_context(struct context *ctx) {
+    NULLIFY(ctx);
+    
     INIT_LIST_HEAD(&ctx->list);
     ctx->irq_registered = false;
+    atomic_set(&ctx->failures_count, 0);
+    atomic_set(&ctx->cycles_count, 0);
+    atomic_set(&ctx->mouse_irqs_count, 0);
+    atomic_set(&ctx->thread_syncs_count, 0);
+    
+    callback(ctx, setup_timer, periodic_timer, periodic_timer_func);
+    callback(ctx, setup_timer, single_shot_timer, timer_func);
+
+    spin_lock_init(&ctx->irq_lock);
+    spin_lock_init(&ctx->tasklet_lock);
+    spin_lock_init(&ctx->irq_tasklet_lock);
+    spin_lock_init(&ctx->tasklet_thread_lock);
+    mutex_init(&ctx->thread_lock);
+    
+    callback(ctx, tasklet_init, tasklet_a, tasklet_func);
+    callback(ctx, tasklet_init, tasklet_b, tasklet_func);
 }
+
+#undef callback
 
 static void free_context(struct context *ctx) {
     if (!ctx) {
@@ -53,6 +234,19 @@ static void free_context(struct context *ctx) {
 
     if (ctx->irq_registered) {
         free_irq(I8042_AUX_IRQ, ctx);
+    }
+
+    tasklet_kill(&ctx->tasklet_a);
+    tasklet_kill(&ctx->tasklet_b);
+
+    del_timer_sync(&ctx->periodic_timer);
+    del_timer_sync(&ctx->single_shot_timer);
+
+    if (ctx->thread_a) {
+        kthread_stop(ctx->thread_a);
+    }
+    if (ctx->thread_b) {
+        kthread_stop(ctx->thread_b);
     }
 
     kfree(ctx);
@@ -72,6 +266,23 @@ static struct context* new_context(void) {
         goto err;
     }
     ctx->irq_registered = true;
+    
+    mod_timer_ms(&ctx->periodic_timer, TIMER_PERIOD_MSEC);
+
+    ctx->thread_a = kthread_create(thread_func, ctx, "r2d2");
+    if (!ctx->thread_a) {
+        ERR("cannot allocate thread a");
+        goto err;
+    }
+
+    ctx->thread_b = kthread_create(thread_func, ctx, "c3p0");
+    if (!ctx->thread_b) {
+        ERR("cannot allocate thread a");
+        goto err;
+    }
+    
+    wake_up_process(ctx->thread_a);
+    wake_up_process(ctx->thread_b);
 
     list_add(&ctx->list, &contexts);
     return ctx;
